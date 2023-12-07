0b5903fb467ff3b6cf5b64521cafa668a91d4176
pandeysp1 <pandeysp1@outlook.com>
2023-12-07 12:23:31 +0530

    added trivy image scan
    added sh based on exit code

---
+++ trivy-docker-image-scan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#trivy-k8s-scan
+
+dockerImagename=$(awk 'NR==1 {print $2}' Dockerfile)
+echo $dockerImageName #getting Image name from env variable
+
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.48.0 -q image --exit-code 0 --severity HIGH --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.48.0 -q image --exit-code 1 --severity CRITICAL --light $imageName
+
+    # Trivy scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
+
+    # Check scan results
+    if [[ ${exit_code} == 1 ]]; then
+        echo "Image scanning failed. Vulnerabilities found"
+        exit 1;
+    else
+        echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    fi;
